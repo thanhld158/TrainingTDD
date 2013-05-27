13822fd0548a7036c24e57e9250c763e695229ae
Le Duy Thanh <leduythanh@HUYPLs-Mac-mini.local>
2013-05-27 14:02:39 +0700

    Initial Commit

---
+++ StringCalculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StringCalculator
+//
+//  Created by Le Duy Thanh on 27/05/2013.
+//  Copyright (c) 2013 Qsoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ StringCalculatorTests/StringCalculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  StringCalculatorTests.m
+//  StringCalculatorTests
+//
+//  Created by Le Duy Thanh on 27/05/2013.
+//  Copyright (c) 2013 Qsoft. All rights reserved.
+//
+
+#import "StringCalculatorTests.h"
+
+@implementation StringCalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in StringCalculatorTests");
+}
+
+@end
