4fbd3fe193c68d106b75dddf6d572ff8815f5f29
yoyo158 <thanhld1590@gmail.com>
2013-06-03 14:43:41 +0700

    Clean

--- StringCalculatorDay6/StringCalculatorDay6/StringCalculatorDay6/StringCalculator.m
+++ StringCalculatorDay6/StringCalculatorDay6/StringCalculatorDay6/StringCalculator.m
@@ -12,19 +12,26 @@
 
 + (int )add:(NSString *)numbers {
     int length = numbers.length;
+    __block int sum = 0;
     
     switch (length) {
         case 0:
-            return 0;
+            sum = 0;
             break;
 
         case 1:
-            return [numbers intValue];
+            sum = [numbers intValue];
             break;
             
-        default:
-            return 3;
+        default: {
+            NSArray *numberArray = [numbers componentsSeparatedByString:@","];
+            [numberArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+                NSString *currentNumber = (NSString *)obj;
+                sum += [currentNumber intValue];
+            }];
             break;
+        }
     }
+    return sum;
 }
 @end
