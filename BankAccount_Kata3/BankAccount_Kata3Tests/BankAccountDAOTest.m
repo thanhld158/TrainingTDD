65eb7439d6c2c1a40a11d08f9b93703d4731e21b
yoyo158 <thanhld1590@gmail.com>
2013-07-23 09:08:06 +0700

    bank account kata 3

--- BankAccount_Kata3/BankAccount_Kata3Tests/BankAccountDAOTest.m
+++ BankAccount_Kata3/BankAccount_Kata3Tests/BankAccountDAOTest.m
@@ -7,8 +7,28 @@
 //
 
 #import "Kiwi.h"
+#import "BankAccountDAO.h"
 #import <LocalStorage/LocalStorage.h>
 
 SPEC_BEGIN(BankAccountDAOTest)
-
+describe(@"BankAccountDAOTest", ^{
+    __block DBEnviroment *env;
+    __block BankAccountDAO *sut;
+    __block NSString *accNumber;
+    
+    beforeEach(^{
+        env = [[DBEnviroment alloc] init];
+        sut = [[BankAccountDAO alloc] init];
+        sut.dataAccessHelper = env.databaseQueue;
+        accNumber = @"1234567890";
+    });
+    
+    afterEach(^{
+        env = nil;
+        sut = nil;
+        accNumber = nil;
+    });
+    
+    it(@"<#string#>", <#^(void)aBlock#>)
+});
 SPEC_END
